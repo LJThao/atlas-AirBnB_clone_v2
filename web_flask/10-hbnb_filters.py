f8424210be371a9badcc4eb77995e0bbcd0545f9
LJThao <yengfong.thao@atlasschool.com>
2024-07-08 00:46:03 -0500

    added files for task 11

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Starts a Flask web app to display HBNB filters"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+
+
+web_app = Flask(__name__)
+
+
+@web_app.teardown_appcontext
+def remove_session(exception):
+    storage.close()
+
+
+@web_app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    states = sorted(storage.all(State).values(), key=lambda state: state.name)
+    cities = sorted(storage.all(City).values(), key=lambda city: city.name)
+    amenities = sorted(storage.all(Amenity).values(), key=lambda amenity: amenity.name)
+    return render_template('10-hbnb_filters.html', states=states, cities=cities, amenities=amenities)
+
+
+if __name__ == '__main__':
+    web_app.run(host='0.0.0.0', port=5000)
