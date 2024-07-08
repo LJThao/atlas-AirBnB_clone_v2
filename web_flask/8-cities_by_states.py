a06079268fa0babd4c7011328d1400663b7592e9
LJThao <yengfong.thao@atlasschool.com>
2024-07-07 22:18:16 -0500

    added task 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Starts a Flask web app for routes cities
+by states list"""
+
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+
+web_app = Flask(__name__)
+
+
+@web_app.teardown_appcontext
+def teardown_appcontext(exception):
+    storage.close()
+
+
+@web_app.route('/cities_by_states')
+def cities_by_states():
+    states = storage.all(State).values()
+    states_sorted = sorted(states, key=lambda state: state.name)
+    return render_template('cities_by_states.html', states=states_sorted)
+
+
+if __name__ == '__main__':
+    web_app.run(host='0.0.0.0', port=5000)
