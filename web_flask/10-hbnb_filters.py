f936178d2afd741408d0d1b6afaa0afa68a6ae40
LJThao <yengfong.thao@atlasschool.com>
2024-07-10 03:15:03 +0000

    update

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -18,9 +18,12 @@ def remove_session(exception):
 
 @web_app.route('/hbnb_filters', strict_slashes=False)
 def hbnb_filters():
+    """Route handler function that requests hbnb filter list"""
+    # sort states, cities, amenities
     states = sorted(storage.all(State).values(), key=lambda state: state.name)
     cities = sorted(storage.all(City).values(), key=lambda city: city.name)
     amenities = sorted(storage.all(Amenity).values(), key=lambda amenity: amenity.name)
+    # render a template with sorted objects
     return render_template('10-hbnb_filters.html', states=states, cities=cities, amenities=amenities)
 
 
