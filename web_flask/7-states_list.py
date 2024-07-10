026702378453fa8aec39f46fbf139910316c06f2
LJThao <yengfong.thao@atlasschool.com>
2024-07-10 01:53:53 +0000

    update states_list func

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,12 +12,18 @@ web_app = Flask(__name__)
 
 @web_app.teardown_appcontext
 def remove_session(exception):
+    """Ensure all open connections are closed"""
     storage.close()
 
 
 @web_app.route('/states_list', strict_slashes=False)
 def states_list():
-    states = storage.all(State)
+    """Route handler function that requests states_list"""
+    # Retrieving all state objects from storage and extracts
+    states = storage.all(State).values()
+    # Sort all state objects
+    states_sorted = sorted(states, key=lambda state: state.name)
+    # Render HTML template by passing dynamic data
     return render_template('7-states_list.html', states=states)
 
 
