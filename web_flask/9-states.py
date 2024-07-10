adb6d1f36ded2a019cf2f3ce40468d49b03a64ce
LJThao <yengfong.thao@atlasschool.com>
2024-07-10 03:13:05 +0000

    update

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -18,18 +18,29 @@ def remove_session(exception):
 
 web_app.route('/states', strict_slashes=False)
 def states_list():
+    """Route handler function that requests states list"""
+    # Retrieve states in database and extract value
     states = storage.all(State).values()
+    # sort all states by name
     states_sorted = sorted(states, key=lambda state: state.name)
+    # render the template with sorted states
     return render_template('9-states.html', states=states_sorted)
 
 
 @web_app.route('/states/<id>', strict_slashes=False)
 def states_cities(id):
+    """Route handler function that requests states, cities, id list"""
+    # Retrieve state from database by id
     state = storage.get("State", id)
+    # if the state object exists(True), then
     if state:
+        # sort all cities name with the state
         cities = sorted(state.cities, key=lambda city: city.name)
+        # render the template with the state and cities sorted
         return render_template('9-states.html', state=state, cities=cities)
+    # if false then
     else:
+        # render the template saying state is not found
         return render_template('9-states.html', state_not_found=True)
 
 
