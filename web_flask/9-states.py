6309fc067379571b52c7e3d9992c0dbc6849cc65
LJThao <yengfong.thao@atlasschool.com>
2024-07-16 02:56:08 +0000

    test

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -5,7 +5,7 @@ by states list"""
 
 from flask import Flask, render_template
 from models import storage
-from models.state import State
+from models import * 
 
 
 web_app = Flask(__name__)
@@ -16,32 +16,14 @@ def remove_session(exception):
     storage.close()
 
 
-web_app.route('/states', strict_slashes=False)
-def states_list():
-    """Route handler function that requests states list"""
-    # Retrieve states in database and extract value
-    states = storage.all(State).values()
-    # sort all states by name
-    states_sorted = sorted(states, key=lambda state: state.name)
-    # render the template with sorted states
-    return render_template('9-states.html', states=states_sorted)
-
-
+@web_app.route('/states', strict_slashes=False)
 @web_app.route('/states/<id>', strict_slashes=False)
-def states_cities(id):
-    """Route handler function that requests states, cities, id list"""
-    # Retrieve state from database by id
-    state = storage.get("State", id)
-    # if the state object exists(True), then
-    if state:
-        # sort all cities name with the state
-        cities = sorted(state.cities, key=lambda city: city.name)
-        # render the template with the state and cities sorted
-        return render_template('9-states.html', state=state, cities=cities)
-    # if false then
-    else:
-        # render the template saying state is not found
-        return render_template('9-states.html', state_not_found=True)
+def states_list(state_id=None):
+    """Route handler function that requests states list"""
+    states = storage.all("State")
+    if state_id is not None:
+        state_id = 'State.' + state_id
+    return render_template('9-states.html', states=states, state_id=state_id)
 
 
 if __name__ == '__main__':
