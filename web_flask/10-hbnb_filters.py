fa927098098a17313ef0d5f085081c40a47dc1fa
LJThao <yengfong.thao@atlasschool.com>
2024-07-16 03:00:01 +0000

    test

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """Starts a Flask web app to display HBNB filters"""
 
+
 from flask import Flask, render_template
 from models import storage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
+from models import *
 
 
 web_app = Flask(__name__)
@@ -20,11 +19,11 @@ def remove_session(exception):
 def hbnb_filters():
     """Route handler function that requests hbnb filter list"""
     # sort states, cities, amenities
-    states = sorted(storage.all(State).values(), key=lambda state: state.name)
-    cities = sorted(storage.all(City).values(), key=lambda city: city.name)
-    amenities = sorted(storage.all(Amenity).values(), key=lambda amenity: amenity.name)
+    states = storage.all("State").values()
+    amenities = storage.all("Amenity").values()
     # render a template with sorted objects
-    return render_template('10-hbnb_filters.html', states=states, cities=cities, amenities=amenities)
+    return render_template('10-hbnb_filters.html', states=states,
+                           amenities=amenities)
 
 
 if __name__ == '__main__':
